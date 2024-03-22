7a8aeda221a9e81d700a62ad0c02e3a61ef17779
Adam Fields <me@aef.me>
2024-03-22 12:43:01 -0400

    Fix spacing

--- scripts/build.ts
+++ scripts/build.ts
@@ -5,7 +5,7 @@ const workers = ['huggingface', 'perplexity']
 
 // ensure each worker builds
 for (let i = 0; i < workers.length; i++) {
-  console.log(`${i > 0 ? '\n ' : ' '}🏗️  Building ${workers[i]} worker...`)
+  console.log(`${i > 0 ? '\n ' : ' '}🏗️ Building ${workers[i]} worker...`)
   const workerPath = resolve(__dirname, `../${workers[i]}/worker.ts`)
   await $`bunx wrangler deploy --dry-run ${workerPath}`
 }

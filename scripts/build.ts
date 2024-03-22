0b0de5a8ada92943e0733c30162fd962573ba4fd
Adam Fields <me@aef.me>
2024-03-22 12:42:02 -0400

    Improve build logging

--- scripts/build.ts
+++ scripts/build.ts
@@ -4,7 +4,8 @@ import { $ } from 'bun'
 const workers = ['huggingface', 'perplexity']
 
 // ensure each worker builds
-for (const worker of workers) {
-  const workerPath = resolve(__dirname, `../${worker}/worker.ts`)
+for (let i = 0; i < workers.length; i++) {
+  console.log(`${i > 0 ? '\n ' : ' '}🏗️  Building ${workers[i]} worker...`)
+  const workerPath = resolve(__dirname, `../${workers[i]}/worker.ts`)
   await $`bunx wrangler deploy --dry-run ${workerPath}`
 }

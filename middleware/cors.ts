23f14b2249ac51f1c8bbe0bbefc8752ee04a7106
Adam Fields <me@aef.me>
2024-03-30 10:25:44 -0400

    Update middleware

--- middleware/cors.ts
+++ middleware/cors.ts
@@ -4,8 +4,8 @@ import type { MiddlewareHandler } from 'hono/types'
 import type { Env } from '../utils/types'
 
 export interface CorsConfig {
-  addMethods?: string[]
-  addHeaders?: string[]
+  methods?: string[]
+  headers?: string[]
 }
 
 /**
@@ -13,19 +13,20 @@ export interface CorsConfig {
  * If not set, defaults `Access-Control-Allow-Origin` to `*`.
  */
 export default function cors(config: CorsConfig = {}): MiddlewareHandler {
-  const { addHeaders = [], addMethods = [] } = config
-  return async (c, next) => {
+  const { headers = [], methods = [] } = config
+
+  return (c, next) => {
     const { ORIGIN, SECRET } = (c.env as Env) || {} // undefined in tests
     const origin = ORIGIN ?? '*'
 
     return corsMiddleware({
       origin,
       credentials: origin === '*' ? false : true,
-      allowMethods: ['GET', 'POST', 'OPTIONS', ...addMethods],
+      allowMethods: ['GET', 'POST', 'OPTIONS', ...methods],
       allowHeaders: [
         origin !== '*' && 'Authorization',
         SECRET && 'X-Api-Key',
-        ...addHeaders
+        ...headers
       ].filter(Boolean)
     })(c, next)
   }
--- middleware/env.ts
+++ middleware/env.ts
@@ -7,13 +7,15 @@ type Var = keyof Env
 
 /** Takes an array of strings and throws a 503 exception if not in `env`. */
 export default function env(vars: Var[]): MiddlewareHandler {
-  return async (c, next) => {
+  return (c, next) => {
     const env = (c.env as Env) || {} // undefined in tests
+
     for (const key of vars) {
       if (!(key in env)) {
         throw new HTTPException(503, { message: 'Service Unavailable' })
       }
     }
-    await next()
+
+    return next()
   }
 }
